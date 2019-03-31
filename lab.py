5524eb052cc81ac5533079ee52e7cbb05f6473dd
srinu274 <nandamsrinivasarao85@gmail.com>
2019-03-31 13:37:00 +0530

    commit 1

---
+++ lab.py
@@ -0,0 +1,28 @@
+def parsePDF(filelocation):
+	pass
+
+def getText(parsedPDF):
+	pass
+	
+def getAuthorInfo(parsedPDF):
+	pass
+
+def getReferences(parsedPDF):
+	pass
+	
+def removespellingmistakes(parsePDF):
+	pass
+
+def crosscheckref(parsedPDF):
+	pass
+
+def storeAndIndex(text,author,references):
+	pass
+
+def visulaizedata():
+	pass
+	
+
+#190511094084
+#https://ntanet.nic.in/ntanet/root/loginpage.aspx
+#https://ntanet.nic.in/NTANETCMS/Handler/FileHandler.ashx?i=File&ii=24&iii=Y
